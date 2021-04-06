efd0d63de8ea0f932fa89af1aa8f6bac8675ac8c
Lance Dang <lancedang64@gmail.com>
2021-04-06 23:56:34 +1000

    Formatted the code

--- myscript.js
+++ myscript.js
@@ -1,133 +1,136 @@
-'use strict'
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
+"use strict";
 
-    function getComputerPlay() {
-        let choice = getRandomInt(3);
-        if (choice == 0) return "Rock";
-        else if (choice == 1) return "Paper";
-        else if (choice == 2) return "Scissors";
-    }
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
-    function playRound(e) {
-        const PLAYER_PLAY  = e.target.id;
-        const COMPUTER_PLAY = getComputerPlay();
-        const ROUND_RESULT = getRoundResult(PLAYER_PLAY, COMPUTER_PLAY);
-        const ROUND_RESULT_TEXT = getResultText(ROUND_RESULT, PLAYER_PLAY, COMPUTER_PLAY);
+function getComputerPlay() {
+  let choice = getRandomInt(3);
+  if (choice == 0) return "Rock";
+  else if (choice == 1) return "Paper";
+  else if (choice == 2) return "Scissors";
+}
 
-        printResult(ROUND_RESULT_TEXT);
-        updateScoreDisplay(ROUND_RESULT);
-        noRound++;        
-        if (playerWins == 5 || playerLosses == 5) endGame();
-    }
+function playRound(e) {
+  const PLAYER_PLAY = e.target.id;
+  const COMPUTER_PLAY = getComputerPlay();
+  const ROUND_RESULT = getRoundResult(PLAYER_PLAY, COMPUTER_PLAY);
+  const ROUND_RESULT_TEXT = getResultText(
+    ROUND_RESULT,
+    PLAYER_PLAY,
+    COMPUTER_PLAY
+  );
 
-    function getRoundResult(playerPlay,computerPlay) {
-        if (playerPlay == computerPlay) {
-            return "draw";
-        }
-        else if (playerPlay == "Rock") {               
-            if (computerPlay == "Scissors") return "win";
-            else return "lose";
-        }
-        else if (playerPlay == "Paper") {
-            if (computerPlay =="Rock") return "win";
-            else return "lose"; 
-        }
-        else if (playerPlay == "Scissors") {
-            if (computerPlay =="Paper") return "win";
-            else return "lose"; 
-        }
-    }
+  printResult(ROUND_RESULT_TEXT);
+  updateScoreDisplay(ROUND_RESULT);
+  noRound++;
+  if (playerWins == 5 || playerLosses == 5) endGame();
+}
 
-    function getResultText(result, playerPlay, computerPlay) {
-        if (result == "draw") {
-            return ` You chose ${playerPlay} and computer chose ${computerPlay}. Draw!`;
-        } else if (result == "win") {
-            return `You chose ${playerPlay} and computer chose ${computerPlay}. You win!`;
-        } else if (result == "lose") {
-            return `You chose ${playerPlay} and computer chose ${computerPlay}. You lose!`;
-        }
-    }
+function getRoundResult(playerPlay, computerPlay) {
+  if (playerPlay == computerPlay) {
+    return "draw";
+  } else if (playerPlay == "Rock") {
+    if (computerPlay == "Scissors") return "win";
+    else return "lose";
+  } else if (playerPlay == "Paper") {
+    if (computerPlay == "Rock") return "win";
+    else return "lose";
+  } else if (playerPlay == "Scissors") {
+    if (computerPlay == "Paper") return "win";
+    else return "lose";
+  }
+}
 
-    function updateScoreDisplay(result) {
-        if (result == "draw") { 
-            playerDraws++;
-            document.getElementById("Draws").textContent = `Draws: ` + `${playerDraws}`;
-        }
-        else if (result == "win") { 
-            playerWins++; 
-            document.getElementById("Wins").textContent = `Wins: ` + `${playerWins}`;
-        }
-        else if (result == "lose") { 
-            playerLosses++;
-            document.getElementById("Losses").textContent = `Losses: ` + `${playerLosses}`;
-        }
-        else if (result == "reset") {
-            
-        }
-    }
+function getResultText(result, playerPlay, computerPlay) {
+  if (result == "draw") {
+    return ` You chose ${playerPlay} and computer chose ${computerPlay}. Draw!`;
+  } else if (result == "win") {
+    return `You chose ${playerPlay} and computer chose ${computerPlay}. You win!`;
+  } else if (result == "lose") {
+    return `You chose ${playerPlay} and computer chose ${computerPlay}. You lose!`;
+  }
+}
 
-    function endGame() {
-        printResult(getGameOverText(playerWins,playerLosses));
-        disablePlayButtons();
-    }
+function updateScoreDisplay(result) {
+  if (result == "draw") {
+    playerDraws++;
+    document.getElementById("Draws").textContent = `Draws: ` + `${playerDraws}`;
+  } else if (result == "win") {
+    playerWins++;
+    document.getElementById("Wins").textContent = `Wins: ` + `${playerWins}`;
+  } else if (result == "lose") {
+    playerLosses++;
+    document.getElementById("Losses").textContent =
+      `Losses: ` + `${playerLosses}`;
+  } else if (result == "reset") {
+  }
+}
 
-    function getGameOverText(playerWins, playerLosses) {
-        if (playerWins == playerLosses) { return "Game over! Draw!"}
-        else if (playerWins > playerLosses)  { return "You are the winner!"}
-        else { return "You lose, the computer is the winner!"}
-    }
+function endGame() {
+  printResult(getGameOverText(playerWins, playerLosses));
+  disablePlayButtons();
+}
 
-    function disablePlayButtons() {
-        document.querySelectorAll("button.playButton").forEach(playButton => {
-            playButton.removeEventListener('click', playRound);
-        });
-    }
+function getGameOverText(playerWins, playerLosses) {
+  if (playerWins == playerLosses) {
+    return "Game over! Draw!";
+  } else if (playerWins > playerLosses) {
+    return "You are the winner!";
+  } else {
+    return "You lose, the computer is the winner!";
+  }
+}
 
-    function printResult(resultText) {
-        const para = document.createElement("p");
-        const gamelog = document.getElementById("gamelog");
+function disablePlayButtons() {
+  document.querySelectorAll("button.playButton").forEach((playButton) => {
+    playButton.removeEventListener("click", playRound);
+  });
+}
 
-        if (playerWins == 5 || playerLosses == 5) {
-            para.style.backgroundColor = "lightgreen";
-            para.style.fontWeight = "bold";
-        } // highlight game over text
-        
-        para.classList.add('singleLog');
-        para.textContent = resultText;
-        gamelog.appendChild(para);
-    }
+function printResult(resultText) {
+  const para = document.createElement("p");
+  const gamelog = document.getElementById("gamelog");
 
-    function resetGame(e) {
-        noRound = 0;
-        playerWins = 0;
-        playerLosses = 0;
-        playerDraws = 0;
-        document.getElementById("Draws").textContent = "Draws: 0";
-        document.getElementById("Wins").textContent = "Wins: 0";
-        document.getElementById("Losses").textContent = "Losses: 0"
-        
-        const playButtons = document.querySelectorAll("button.playButton");
-        playButtons.forEach(playButton => {
-            playButton.addEventListener("click", playRound);
-        });
+  if (playerWins == 5 || playerLosses == 5) {
+    para.style.backgroundColor = "lightgreen";
+    para.style.fontWeight = "bold";
+  } // highlight game over text
 
-        const oldGameLogs = document.querySelectorAll(".singleLog");
-        oldGameLogs.forEach(singleLog => {
-            singleLog.remove();
-        });
-    }
+  para.classList.add("singleLog");
+  para.textContent = resultText;
+  gamelog.appendChild(para);
+}
 
-    let playerWins = 0;
-    let playerLosses = 0;
-    let playerDraws = 0;
-    let noRound = 0;
+function resetGame(e) {
+  noRound = 0;
+  playerWins = 0;
+  playerLosses = 0;
+  playerDraws = 0;
+  document.getElementById("Draws").textContent = "Draws: 0";
+  document.getElementById("Wins").textContent = "Wins: 0";
+  document.getElementById("Losses").textContent = "Losses: 0";
 
-    const playButtons = document.querySelectorAll("button.playButton");
-    playButtons.forEach(playButton => {
-        playButton.addEventListener("click", playRound);
-    });
+  const playButtons = document.querySelectorAll("button.playButton");
+  playButtons.forEach((playButton) => {
+    playButton.addEventListener("click", playRound);
+  });
 
-    const resetButton = document.querySelector("button.resetButton");
-    resetButton.addEventListener("click", resetGame);
+  const oldGameLogs = document.querySelectorAll(".singleLog");
+  oldGameLogs.forEach((singleLog) => {
+    singleLog.remove();
+  });
+}
+
+let playerWins = 0;
+let playerLosses = 0;
+let playerDraws = 0;
+let noRound = 0;
+
+const playButtons = document.querySelectorAll("button.playButton");
+playButtons.forEach((playButton) => {
+  playButton.addEventListener("click", playRound);
+});
+
+const resetButton = document.querySelector("button.resetButton");
+resetButton.addEventListener("click", resetGame);
